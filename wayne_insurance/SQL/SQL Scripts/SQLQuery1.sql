1a43ddae81c11478eb133f453c8af122af6b25db
natureboy1976 <wfrancis4@hotmail.com>
2014-09-27 18:18:38 +0100

    Customer table creation
    The customer table created.

--- wayne_insurance/SQL/SQL Scripts/SQLQuery1.sql
+++ wayne_insurance/SQL/SQL Scripts/SQLQuery1.sql
@@ -1,2 +1,13 @@
 CREATE DATABASE wayneinsurance;
 GO
+USE wayneinsurance;
+CREATE TABLE Customers (
+CustomerID INT PRIMARY KEY NOT NULL,
+Title VARCHAR (10),
+Firstname VARCHAR (25) NOT NULL,
+Lastname VARCHAR (25) NOT NULL,
+Date_of_Birth DATETIME,
+Phone_number VARCHAR (25) NOT NULL,
+Email VARCHAR (50) NOT NULL
+);
+GO
