1f54e93445b187dab13c6f46762c1ea4c82063a4
nunosn <nunosn@yahoo.com.br>
2014-09-27 18:28:36 +0100

    Deleted Additional pages (contact, about us)
    Have deleted redundant/unnecessary pages

--- wayne_insurance/wayne_insurance/Controllers/HomeController.cs
+++ wayne_insurance/wayne_insurance/Controllers/HomeController.cs
@@ -14,27 +14,5 @@ namespace wayne_insurance.Controllers
 
             return View();
         }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your app description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-
-            public ActionResult items()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-
-
-        }
     }
 }
